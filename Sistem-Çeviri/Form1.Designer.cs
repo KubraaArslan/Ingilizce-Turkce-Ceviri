8518ac310de8ac507c366de1d6b58ae429db91ad
Kübra Arslan <kubrarslan0606@gmail.com>
2023-07-18 15:50:55 +0300

    Ingilizce-Turkce-Ceviri

---
+++ Sistem-Çeviri/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Sistem_Çeviri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOku2 = new System.Windows.Forms.Button();
+            this.btnOku1 = new System.Windows.Forms.Button();
+            this.btnCevir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOku2
+            // 
+            this.btnOku2.Location = new System.Drawing.Point(398, 183);
+            this.btnOku2.Name = "btnOku2";
+            this.btnOku2.Size = new System.Drawing.Size(75, 23);
+            this.btnOku2.TabIndex = 2;
+            this.btnOku2.Text = "OKU2";
+            this.btnOku2.UseVisualStyleBackColor = true;
+            this.btnOku2.Click += new System.EventHandler(this.btnOku2_Click);
+            // 
+            // btnOku1
+            // 
+            this.btnOku1.Location = new System.Drawing.Point(30, 183);
+            this.btnOku1.Name = "btnOku1";
+            this.btnOku1.Size = new System.Drawing.Size(75, 23);
+            this.btnOku1.TabIndex = 3;
+            this.btnOku1.Text = "OKU1";
+            this.btnOku1.UseVisualStyleBackColor = true;
+            this.btnOku1.Click += new System.EventHandler(this.btnOku1_Click);
+            // 
+            // btnCevir
+            // 
+            this.btnCevir.Location = new System.Drawing.Point(317, 70);
+            this.btnCevir.Name = "btnCevir";
+            this.btnCevir.Size = new System.Drawing.Size(75, 77);
+            this.btnCevir.TabIndex = 4;
+            this.btnCevir.Text = "<->";
+            this.btnCevir.UseVisualStyleBackColor = true;
+            this.btnCevir.Click += new System.EventHandler(this.btnCevir_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 45);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 132);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(398, 45);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(273, 132);
+            this.textBox2.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "İngilizce";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(395, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Türkçe";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 293);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCevir);
+            this.Controls.Add(this.btnOku1);
+            this.Controls.Add(this.btnOku2);
+            this.Name = "Form1";
+            this.Text = "Türkçe-İngilizce";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOku2;
+        private System.Windows.Forms.Button btnOku1;
+        private System.Windows.Forms.Button btnCevir;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Sistem-Çeviri/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistem_Çeviri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        WebBrowser ceviri = new WebBrowser();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ceviri.Navigate("https://www.bing.com/translator?to=tr&setlang=tr");
+                ceviri.ScriptErrorsSuppressed = true;
+                //Bing translate yi webbrowser ile aç
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            ceviri.Document.GetElementById("tta_input_ta").InnerText = textBox1.Text;
+            timer1.Start();
+            //TextBox1 e yazı yazılırken çevir.
+        }
+
+        private void btnOku1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ceviri.Document.GetElementById("tta_playiconsrc").InvokeMember("click");
+                //Girilen kelimeyi oku ---> oku1
+                
+            }
+            catch
+            {
+
+            }
+        }
+        int say = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {   if (say == 2) timer1.Stop();
+                textBox2.Text = ceviri.Document.GetElementById("tta_output_ta").InnerText;
+                say++;
+                //Çevirilen kelimeyi textbox2ye getir
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnOku2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ceviri.Document.GetElementById("tta_playicontgt").InvokeMember("click");
+                //çevirilen kelimeyi oku
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void btnCevir_Click(object sender, EventArgs e)
+        {
+            try
+            {   if (label1.Text == "Türkçe") { label1.Text = "İngilizce"; label2.Text = "Türkçe"; }
+                else { label2.Text = "İngilizce"; label1.Text = "Türkçe"; }
+                ceviri.Document.GetElementById("tta_revIcon").InvokeMember("click");
+                // Dilleri değiştir
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
